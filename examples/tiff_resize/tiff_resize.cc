1b3f876d6ff61d7b18594769685aac9b5787b4cd
Syoyo Fujita <syoyo@lighttransport.com>
2017-10-06 14:20:52 +0900

    Consider Bayer pattern when resizing.

--- examples/tiff_resize/tiff_resize.cc
+++ examples/tiff_resize/tiff_resize.cc
@@ -70,10 +70,11 @@ int
 main(int argc, char **argv)
 {
   size_t image_idx = static_cast<size_t>(-1); // -1 = use largest image.
-  if (argc < 3) {
-    std::cout << "tiff_resize input.tiff output.tiff resize_factor (image_idx)" << std::endl;
+  if (argc < 4) {
+    std::cout << "tiff_resize input.tiff output.tiff resize_factor (bayer) (image_idx)" << std::endl;
     std::cout << "  options" << std::endl;
     std::cout << "    resize_factor       : Resize image to (1/resize_factor). e.g, 2: half size, 4: 1/4 size." << std::endl;
+    std::cout << "    bayer               : 1:Keep 2x2 Bayer for resizing image. 0:Do not consider Bayer pattern for resizing." << std::endl;
     std::cout << "    image_idx           : The index of image for conversion. Default = auto detect(choose the largest image in TIFF)." << std::endl;
     return EXIT_FAILURE;
   }
@@ -93,8 +94,13 @@ main(int argc, char **argv)
     return EXIT_FAILURE;
   }
 
+  bool bayer = false;
   if (argc > 4) {
-    image_idx = static_cast<size_t>(atoi(argv[4]));
+    bayer = bool(atoi(argv[4]));
+  }
+
+  if (argc > 5) {
+    image_idx = static_cast<size_t>(atoi(argv[5]));
   }
 
   std::vector<tinydng::DNGImage> images;
@@ -157,33 +163,93 @@ main(int argc, char **argv)
 
     const unsigned short *image_data = reinterpret_cast<const unsigned short *>(image.data.data());
 
-    const size_t step = size_t(resize_factor);
-    const size_t dst_width = width / size_t(resize_factor);
-    const size_t dst_height = height / size_t(resize_factor);
+    size_t step = size_t(resize_factor);
+    size_t dst_width = width / size_t(resize_factor);
+    size_t dst_height = height / size_t(resize_factor);
     if ((dst_width < 2) || (dst_height < 2)) {
       std::cerr << "Resized image too small." << std::endl;
       return EXIT_FAILURE;
     }
 
+    if (bayer) {
+      // multiple of 2.
+      dst_width = (2 * dst_width) / 2;
+      dst_height = (2 * dst_height) / 2;
+    }
+
     std::vector<unsigned short> buf;
     buf.resize(dst_width * dst_height * size_t(image.samples_per_pixel));
+    memset(buf.data(), 0, sizeof(dst_width * dst_height * size_t(image.samples_per_pixel) * sizeof(unsigned short)));
 
     if (image.samples_per_pixel == 1) {
-
-      for (size_t y = 0, v = 0; y < height; y+=step, v++) {
-        for (size_t x = 0, u = 0; x < width; x+=step, u++) {
-          buf[v * dst_width + u] = image_data[y * width + x];
-          //printf("[%d, %d] = %d, width %d\n", int(u), int(v), buf[v * dst_width + u], int(dst_width));
+      if (bayer) {
+        // Assume 2x2 Bayer
+        step *= 2;
+        for (size_t y = 0, v = 0; y < height; y+=step, v+=2) {
+          for (size_t x = 0, u = 0; x < width; x+=step, u+=2) {
+            for (size_t t = 0; t < 2; t++) {
+              if ((v + t) >= dst_height) {
+                continue;
+              }
+              for (size_t s = 0; s < 2; s++) {
+                if ((u + s) >= dst_width) {
+                  continue;
+                }
+                unsigned short val = 0;
+                if (((y + t) < height) && ((x + s) < width)) {
+                  val = image_data[(y + t) * width + (x + s)];
+                }
+                buf[(v + t) * dst_width + (u + s)] = val;
+                //printf("[%d, %d] = %d, width %d\n", int(u), int(v), buf[v * dst_width + u], int(dst_width));
+              }
+            }
+          }
+        }
+      } else {
+        for (size_t y = 0, v = 0; y < height; y+=step, v++) {
+          for (size_t x = 0, u = 0; x < width; x+=step, u++) {
+            buf[v * dst_width + u] = image_data[y * width + x];
+            //printf("[%d, %d] = %d, width %d\n", int(u), int(v), buf[v * dst_width + u], int(dst_width));
+          }
         }
       }
 
     } else if (image.samples_per_pixel == 3) {
-
-      for (size_t y = 0, v = 0; y < height; y+=step, v++) {
-        for (size_t x = 0, u = 0; x < width; x+=step, u++) {
-          buf[3 * (v * dst_width + u) + 0] = image_data[3 * (v * width + u) + 0];
-          buf[3 * (v * dst_width + u) + 1] = image_data[3 * (v * width + u) + 1];
-          buf[3 * (v * dst_width + u) + 2] = image_data[3 * (v * width + u) + 2];
+      if (bayer) {
+        // Assume 2x2 Bayer
+        step *= 2;
+        for (size_t y = 0, v = 0; y < height; y+=step, v+=2) {
+          for (size_t x = 0, u = 0; x < width; x+=step, u+=2) {
+            for (size_t t = 0; t < 2; t++) {
+              if ((v + t) >= dst_height) {
+                continue;
+              }
+              for (size_t s = 0; s < 2; s++) {
+                if ((u + s) >= dst_width) {
+                  continue;
+                }
+                unsigned short val0 = 0;
+                unsigned short val1 = 0;
+                unsigned short val2 = 0;
+                if (((y + t) < height) && ((x + s) < width)) {
+                  val0 = image_data[3 * ((y + t) * width + (x + s)) + 0];
+                  val1 = image_data[3 * ((y + t) * width + (x + s)) + 1];
+                  val2 = image_data[3 * ((y + t) * width + (x + s)) + 2];
+                }
+                buf[3 * ((v + t) * dst_width + (u + s)) + 0] = val0;
+                buf[3 * ((v + t) * dst_width + (u + s)) + 1] = val1;
+                buf[3 * ((v + t) * dst_width + (u + s)) + 2] = val2;
+              }
+            }
+          }
+        }
+      } else {
+        for (size_t y = 0, v = 0; y < height; y+=step, v++) {
+          for (size_t x = 0, u = 0; x < width; x+=step, u++) {
+            buf[3 * (v * dst_width + u) + 0] = image_data[3 * (v * width + u) + 0];
+            buf[3 * (v * dst_width + u) + 1] = image_data[3 * (v * width + u) + 1];
+            buf[3 * (v * dst_width + u) + 2] = image_data[3 * (v * width + u) + 2];
+          }
         }
       }
 
