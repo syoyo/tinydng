35592adc5d7109fc02801c72fdb1ef441e2591c1
Syoyo Fujita <syoyo@lighttransport.com>
2022-12-22 02:13:10 +0900

    decode 12 bit and 14 bit image to 16bit image.

--- experimental/python/simple_load.py
+++ experimental/python/simple_load.py
@@ -4,3 +4,8 @@ import tinydng
 images = tinydng.loaddng("../../colorchart.dng")
 
 print(len(images))
+
+for img in images:
+    print("width:", img.width)
+    print("height:", img.height)
+    print("bits per sample:", img.bits_per_sample)
